88c1050cf30ed378060ed45f94b2961960b7d054
srija0302 <srijareddy32000@gmail.com>
2024-01-30 14:47:34 +0530

    hashing passwords before storing to db

--- server/registerUser/index.js
+++ server/registerUser/index.js
@@ -1,6 +1,8 @@
-const pool = require('../connection-pool')
+const pool = require('../connection-pool');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const registerUser = (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({ success: false, message: 'Email and password are required' });
@@ -11,21 +13,35 @@ const registerUser = (req, res) => {
       console.error('Error getting MySQL connection:', err);
       return res.status(500).json({ success: false, message: 'Internal server error' });
     }
-
-    const query = 'INSERT INTO users (Email, Password) VALUES (?, ?)';
-    connection.query(query, [email, password], (error, results) => {
-
-      connection.release();
-
-      if (error) {
-        console.error('Error executing MySQL query:', error);
-        return res.status(500).json({ success: false, message: 'Internal server error' });
+    const plainTextPassword = password;
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-      if(results){
-      res.json({ success: true, message: 'User registered successfully' });
-      }
+      bcrypt.hash(plainTextPassword, salt, (err, hash) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const query = 'INSERT INTO users (Email, Password) VALUES (?, ?)';
+        connection.query(query, [email, hash], (error, results) => {
+
+          connection.release();
+
+          if (error) {
+            console.error('Error executing MySQL query:', error);
+            return res.status(500).json({ success: false, message: 'Internal server error' });
+          }
+          if (results) {
+            res.json({ success: true, message: 'User registered successfully' });
+          }
+        });
+      });
     });
-});
+
+  });
 
 }
 
